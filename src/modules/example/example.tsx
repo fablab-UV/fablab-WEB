4b51afb819328e40e616855c5c07c8763de868d1
Ixyz.dev <ixyz.dev@gmail.com>
2024-06-21 19:33:58 -0400

    Quitando eslint

---
+++ src/modules/example/example.tsx
@@ -0,0 +1,9 @@
+// Es el componente contenedor que importa y compone los subcomponentes del módulo.
+// Este archivo actúa como la interfaz pública del módulo.
+import React from 'react'
+
+const example = () => {
+  return <div>example</div>
+}
+
+export default example
---
+++ src/modules/example/exampleService.tsx
@@ -0,0 +1,10 @@
+// Maneja las llamadas a la API y cualquier otra interacción
+// externa necesaria para obtener los datos o realizar
+// acciones específicas relacionadas con el módulo.
+import React from 'react'
+
+const exampleService = () => {
+  return <div>exampleService</div>
+}
+
+export default exampleService
