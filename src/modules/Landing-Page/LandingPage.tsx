59992aae18fffa7e7559fb5a152c4d628e9c80ab
Ixyz.dev <ixyz.dev@gmail.com>
2024-06-25 19:04:38 -0400

    Implementando base

--- src/modules/Landing-Page/LandingPage.tsx
+++ src/modules/Landing-Page/LandingPage.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 // Import del layout
 import MainLayout from '@/layouts/Main-Layout/MainLayout'
+import YoutubeVideo from '../youtube-video/YoutubeVideo'
 
 const LandingPage: React.FC = () => {
   return (
     <MainLayout>
       <div>LandingPage</div>
+      <YoutubeVideo />
     </MainLayout>
   )
 }
