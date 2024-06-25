9d7750c88462a0a3b13381c0356987243aa1c600
Ixyz.dev <ixyz.dev@gmail.com>
2024-06-25 18:58:23 -0400

    Implementando base

--- src/modules/Landing-Page/LandingPage.tsx
+++ src/modules/Landing-Page/LandingPage.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 // Import del layout
 import MainLayout from '@/layouts/Main-Layout/MainLayout'
+import Contact from '../contact/Contact'
 
 const LandingPage: React.FC = () => {
   return (
     <MainLayout>
       <div>LandingPage</div>
+      <Contact />
     </MainLayout>
   )
 }
