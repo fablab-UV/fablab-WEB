7f62962a974de85f4147ceeee837ea78e2e5c193
Ixyz.dev <ixyz.dev@gmail.com>
2024-07-16 21:32:49 -0400

    Concluido

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
 import '@/styles/globals.css'
+
 import { cn } from '@/lib/utils'
 
 const fontSans = FontSans({
