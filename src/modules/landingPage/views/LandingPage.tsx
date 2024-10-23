5c76b328d5a4c6c7ed591a802ad21ace855fee9a
Ixyz.Dev <ixyz.dev@gmail.com>
2024-10-23 12:45:44 -0300

    Implementando base

--- src/modules/landingPage/views/LandingPage.tsx
+++ src/modules/landingPage/views/LandingPage.tsx
@@ -6,12 +6,14 @@ import { MainLayout } from '@/layouts'
 // Importa los componentes que compondran la vista
 import Slider from '@/components/common/carousel/Slider'
 import 'keen-slider/keen-slider.min.css'
+import Contact from '@/modules/contacts/Contact'
 
 const LandingPage: React.FC = () => {
   return (
     <MainLayout>
       <div className="w-full">
         <Slider h="7" />
+        <Contact />
       </div>
     </MainLayout>
   )
