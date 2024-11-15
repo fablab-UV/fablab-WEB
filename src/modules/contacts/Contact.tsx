8b227c5ce02cc801be6c1fadfebbbc9be8095412
Ixyz.Dev <ixyz.dev@gmail.com>
2024-11-15 20:35:13 -0300

    Implementando mejoras en husky

--- src/modules/contacts/Contact.tsx
+++ src/modules/contacts/Contact.tsx
@@ -1,6 +1,6 @@
-'use client';
-import React, { useState, useRef } from 'react';
-import emailjs from 'emailjs-com';
+'use client'
+import React, { useState, useRef } from 'react'
+import emailjs from 'emailjs-com'
 
 // Componente principal Contact
 const Contact: React.FC = () => {
@@ -8,73 +8,79 @@ const Contact: React.FC = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
-    message: '',
-  });
-  
+    message: ''
+  })
+
   // Estado que indica si el formulario ha sido enviado con éxito
-  const [submitted, setSubmitted] = useState(false);
-  
+  const [submitted, setSubmitted] = useState(false)
+
   // Estado para manejar los mensajes de error en el formulario
-  const [error, setError] = useState('');
-  
+  const [error, setError] = useState('')
+
   // Referencia para el botón de envío, que se puede utilizar para hacer scroll hacia él
-  const submitButtonRef = useRef<HTMLButtonElement>(null);
-  
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const submitButtonRef = useRef<HTMLButtonElement>(null)
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
 
   // Maneja el cambio de los campos del formulario
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
-    const { name, value } = e.target;
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void => {
+    const { name, value } = e.target
     setFormData({
       ...formData,
-      [name]: value,
-    });
+      [name]: value
+    })
 
     // Al cambiar el valor, hace scroll automático hacia el botón de envío
     if (submitButtonRef.current != null) {
-      submitButtonRef.current.scrollIntoView({ behavior: 'smooth' });
+      submitButtonRef.current.scrollIntoView({ behavior: 'smooth' })
     }
-  };
+  }
 
   // Función de envío del formulario
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
+    e.preventDefault()
 
     // Validación: revisa si algún campo está vacío y muestra un mensaje de error
-    if (formData.name.length === 0 || formData.email.length === 0 || formData.message.length === 0) {
-      setError('Por favor, completa todos los campos.');
-      return;
+    if (
+      formData.name.length === 0 ||
+      formData.email.length === 0 ||
+      formData.message.length === 0
+    ) {
+      setError('Por favor, completa todos los campos.')
+      return
     }
 
-    setIsSubmitting(true);
+    setIsSubmitting(true)
 
     // Configura los parámetros para enviar el formulario con EmailJS
     emailjs
       .send(
-        'tu_service_id',       // ID de tu servicio (reemplaza esto)
-        'tu_template_id',      // ID de tu plantilla (reemplaza esto)
-        formData,              // Datos del formulario
-        'tu_public_key'        // Clave pública de EmailJS (reemplaza esto)
+        'tu_service_id', // ID de tu servicio (reemplaza esto)
+        'tu_template_id', // ID de tu plantilla (reemplaza esto)
+        formData, // Datos del formulario
+        'tu_public_key' // Clave pública de EmailJS (reemplaza esto)
       )
       .then((result) => {
-        console.log(result.text);
-        setSubmitted(true);
-        setError('');
+        console.log(result.text)
+        setSubmitted(true)
+        setError('')
         setFormData({
           name: '',
           email: '',
-          message: '',
-        });
+          message: ''
+        })
       })
       .catch((error) => {
-        console.error(error.text);
-        setError('Hubo un error al enviar el mensaje.');
+        console.error(error.text)
+        setError('Hubo un error al enviar el mensaje.')
       })
       .finally(() => {
-        setIsSubmitting(false);
-      });
-  };
-  
+        setIsSubmitting(false)
+      })
+  }
+
   return (
     <section className="text-gray-600 body-font relative">
       <div className="container px-5 py-16 mx-auto flex sm:flex-nowrap flex-wrap">
@@ -88,23 +94,34 @@ const Contact: React.FC = () => {
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
           <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-white bg-opacity-75 text-center px-4 py-2 rounded-md shadow-md">
-            <p className="text-gray-800 font-medium">Gral Cruz 222, FabLab UV</p>
+            <p className="text-gray-800 font-medium">
+              Gral Cruz 222, FabLab UV
+            </p>
           </div>
         </div>
 
         {/* Formulario de contacto en la sección derecha */}
         <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col w-full h-[600px] p-0">
-          <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Contáctanos</h2>
+          <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+            Contáctanos
+          </h2>
           <p className="leading-relaxed mb-5 text-gray-600">
-            Si deseas agendar una visita o realizar una consulta, rellena el formulario que se encuentra a continuación.
+            Si deseas agendar una visita o realizar una consulta, rellena el
+            formulario que se encuentra a continuación.
           </p>
           <p className="leading-relaxed mb-5 text-gray-600">
-            IMPORTANTE: horario de atención a público general (no residentes ni becados) es de 14:00 a 18:00hrs.
+            IMPORTANTE: horario de atención a público general (no residentes ni
+            becados) es de 14:00 a 18:00hrs.
           </p>
-          <form onSubmit={handleSubmit} className="flex flex-col justify-between">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col justify-between"
+          >
             {/* Campo de Nombre */}
             <div className="relative mb-4">
-              <label htmlFor="name" className="leading-7 text-sm text-gray-600">Nombre</label>
+              <label htmlFor="name" className="leading-7 text-sm text-gray-600">
+                Nombre
+              </label>
               <input
                 type="text"
                 id="name"
@@ -116,7 +133,12 @@ const Contact: React.FC = () => {
             </div>
             {/* Campo de Correo Electrónico */}
             <div className="relative mb-4">
-              <label htmlFor="email" className="leading-7 text-sm text-gray-600">Correo electrónico</label>
+              <label
+                htmlFor="email"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Correo electrónico
+              </label>
               <input
                 type="email"
                 id="email"
@@ -128,7 +150,12 @@ const Contact: React.FC = () => {
             </div>
             {/* Campo de Mensaje */}
             <div className="relative mb-4">
-              <label htmlFor="message" className="leading-7 text-sm text-gray-600">Tu mensaje:</label>
+              <label
+                htmlFor="message"
+                className="leading-7 text-sm text-gray-600"
+              >
+                Tu mensaje:
+              </label>
               <textarea
                 id="message"
                 name="message"
@@ -138,7 +165,7 @@ const Contact: React.FC = () => {
               ></textarea>
             </div>
             {/* Mensaje de error */}
-            {(error.length > 0) && <p className="text-red-500 mt-3">{error}</p>}
+            {error.length > 0 && <p className="text-red-500 mt-3">{error}</p>}
             {/* Botón de Enviar */}
             <button
               ref={submitButtonRef}
@@ -151,14 +178,18 @@ const Contact: React.FC = () => {
             </button>
             {/* Mensaje de confirmación al enviar */}
             {submitted && (
-              <p className="text-green-500 mt-3">¡Tu mensaje ha sido enviado correctamente!</p>
+              <p className="text-green-500 mt-3">
+                ¡Tu mensaje ha sido enviado correctamente!
+              </p>
             )}
-            <p className="text-xs text-gray-500 mt-3">Tiempo estimado de respuesta: 24 horas hábiles.</p>
+            <p className="text-xs text-gray-500 mt-3">
+              Tiempo estimado de respuesta: 24 horas hábiles.
+            </p>
           </form>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Contact;
+export default Contact
